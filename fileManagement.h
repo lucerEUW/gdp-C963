b1b4e7572e9ec8b21ef433c6a7b914910cf7ab45
lucerEUW <149318953+lucerEUW@users.noreply.github.com>
2024-02-01 21:41:45 +0100

    Update fileManagement.h

--- fileManagement.h
+++ fileManagement.h
@@ -1,16 +1,26 @@
+#ifndef FILEMANAGER_H
+#define FILEMANAGER_H
+
+#include<string>
 #include<fstream>
 #include<cctype>
 #include<algorithm>
 #include<limits>
+#include<iomanip>
 #include"book.h"
-
 
 namespace manage
 {
-  std::string getExactLine(std::string filename,  int line);
+  std::string getExactLine(const std::string& filename,  int line);
+
   std::string readAmount(std::string targetFile);
+  void UpdateAmount(std::string targetFile,  int action, int amount);
+
   std::string readCounter();
+  void UpdateCounter(int action, int amount);
+
   std::string toLower(std::string searchTerm);
+
   std::string getSearchTerm();
   void Search();
 }
