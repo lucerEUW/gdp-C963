d814db5b2c25a3d890e53bf2902d555b63892373
lucerEUW <149318953+lucerEUW@users.noreply.github.com>
2024-02-06 18:48:49 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,21 +11,24 @@ int main()
   char    choice;
   bool contRunning  = true;
 
-  while(contRunning){
+  while(contRunning)
+  {
+    std::string copies  = "[" + manage::readCounter() + " copies]";
     std::cout <<  "=========================================================" << std::endl
-              <<  "Buecherverwaltung" <<  std::endl
+              <<  std::setw(12) <<  std::left << "Bookmanager" << std::setw(45) << std::right << copies  <<  std::endl
               <<  "=========================================================" << std::endl
-              <<  std::setw(25) <<  std::left <<  "s:search/restock/buy"  <<  std::setw(13) << std::right <<"a:add new  q:quit"  <<  std::setw(5) << std::right <<  "[" <<  manage::readCounter()  <<  " copies]"  <<  std::endl
+              <<  std::setw(25) <<  std::left <<  "s:search/restock/buy"  <<  std::setw(14) << std::internal <<"a:add new"  <<  std::setw(18) <<  std::right  <<  "q:quit"   <<  std::endl
               <<  "=========================================================" << std::endl;
     std::cin  >>  choice;
 
-    switch (choice) {
+    switch (choice)
+    {
       case  's':
         manage::Search();
         break;
       case  'a':
         {
-          std::cout <<  "give dates of book to add [titel autor verlag isbn preis anzahl]: ";
+          std::cout <<  "give data for book to add [titel autor verlag isbn preis anzahl]: ";
           std::cin  >>  titel >>  autor >>  verlag  >>  isbn  >>  preis >>  amount;
           Book newBook(titel, autor, verlag, isbn, preis,  amount);
           newBook.Add();
